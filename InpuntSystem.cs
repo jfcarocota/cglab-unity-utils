6852ce1d78e73cc842289737a1ac6939556c2132
jfcarocota <jfcarocota@gmail.com>
2019-09-13 11:20:32 -0700

    InpuntSystem summary

--- InpuntSystem.cs
+++ InpuntSystem.cs
@@ -5,6 +5,9 @@ namespace CGLabUtils.InpuntSystem
 {
     public class InpuntSystem 
     {
+        ///<summary>
+        ///This functions returns the vector2 Axis direction.
+        ///</summary>
         public static Vector2 Axis
         {
             get => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
