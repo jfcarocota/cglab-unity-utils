1dbe4ac2fc8ac2ecc92b3ab3c4b43ac98ff692d2
jfcarocota <jfcarocota@gmail.com>
2019-09-13 11:19:23 -0700

    InpuntSystem agregado

---
+++ InpuntSystem.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+namespace CGLabUtils.InpuntSystem
+{
+    public class InpuntSystem 
+    {
+        public static Vector2 Axis
+        {
+            get => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        }
+    }
+}
